5205d3d55983bbc0d8bfb2fede60b6c5397be446
zahangir710 <zahangir710@gmail.com>
2021-09-25 21:08:25 +0200

    first commit

---
+++ src/component/Header/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Header.css";
+const Header = () => {
+  return (
+    <div>
+      <nav>
+        <a href="/Home">Home</a>
+        <a href="/Features">Features</a>
+        <a href="/Events">Events</a>
+      </nav>
+      <h1>World Leader's Chess Competition 2021</h1>
+      <h4>Build Your Dream Chess Team</h4>
+    </div>
+  );
+};
+
+export default Header;
